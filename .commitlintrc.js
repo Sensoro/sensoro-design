e5d292e903bb3dd72476d040800d4c86712b2ff4
wangxingkang <156148958@qq.com>
2021-12-13 11:34:19 +0800

    🔧 chore: add commitlint config

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ * @see https://commitlint.js.org/#/reference-configuration
+ */
+const Configuration = {
+  extends: ['@walrus/commitlint-config-emoji']
+};
+
+module.exports = Configuration;
