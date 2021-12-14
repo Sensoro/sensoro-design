5d71492e2fefc6cf52f91f6101a338cd20b271c9
wangxingkang <156148958@qq.com>
2021-12-14 15:24:05 +0800

    ✨ feat(icon): 添加新 Icon

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,13 +1,5 @@
-import { SFC } from 'react';
 import createFromIconfontCN from '@ant-design/icons/es/components/IconFont';
-import { IconBaseProps } from '@ant-design/icons/es/components/Icon';
 
-export interface IconFontProps extends IconBaseProps {
-  type: string;
-}
-
-const Icon: SFC<IconFontProps> = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1720118_lv0bz31erj.js'
+export default createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1720118_ebn9fjph9a6.js'
 });
-
-export default Icon;
