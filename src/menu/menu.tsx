d58094e5756e0c785a72b9c5c7bd97f0a9c7da2d
wangxingkang <156148958@qq.com>
2021-12-17 16:57:33 +0800

    🏷️ chore: 暂时修复类型报错

--- src/menu/menu.tsx
+++ src/menu/menu.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import classNames from "@pansy/classnames";
-import { Menu as AntMenu } from "antd";
-import { MenuProps } from "antd/es/menu";
-import "./style/index.less";
+import React from 'react';
+import classNames from '@pansy/classnames';
+import { Menu as AntMenu } from 'antd';
+import { MenuProps } from 'antd/es/menu';
+import './style/index.less';
 const { SubMenu, Divider, Item, ItemGroup } = AntMenu;
-const prefixCls = "sen-menu";
+const prefixCls = 'sen-menu';
 
 export default class Menu extends React.Component<MenuProps, {}> {
-  static Divider = Divider;
+  static Divider = Divider as any;
 
   static Item = Item;
 
   static SubMenu = SubMenu;
 
-  static ItemGroup = ItemGroup;
+  static ItemGroup = ItemGroup as any;
 
   render() {
     const { className, ...rest } = this.props;
