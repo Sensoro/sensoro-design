d811626dbc784c1b922264b0351123a7a5136028
wangxingkang <wang_xingkang@qq.com>
2022-02-08 17:13:22 +0800

    📝 docs: replace logo url

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,15 @@
 import { join } from 'path';
 
 const baseUrl = '/sensoro-design';
+const logo = 'https://cdn.jsdelivr.net/gh/wangxingkang/pictures@latest/imgs/sensoro-design.svg';
 
 export default {
   mode: 'site',
   title: 'Sensoro Design',
   base: baseUrl,
   publicPath: `${baseUrl}/`,
-  favicon: `${baseUrl}/logo.svg`,
-  logo: `${baseUrl}/logo.svg`,
+  favicon: logo,
+  logo,
   resolve: {
     includes: ['docs']
   },
